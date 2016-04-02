579ff739ca5ebce082b972d53931c4bd73657155
marcoaam <marco.army@gmail.com>
2016-04-02 14:54:30 +0100

    Refactored the queries response handle

--- api.js
+++ api.js
@@ -4,21 +4,42 @@ var status = require('http-status');
 module.exports = function(wagner) {
   var api = express.Router();
 
+  var handleOne = function(property, res, error, result) {
+      if(error) {
+        return res.
+          status(status.INTERNAL_SERVER_ERROR).
+          json({ error: error.toString() });
+      }
+
+      if(!result) {
+        return res.
+          status(status.NOT_FOUND).
+          json({ error: 'Not found' });
+      }
+
+      var json = {};
+      json[property] = result;
+      res.json(json);
+  };
+
+  var handleMany = function(property, res, error, result) {
+    if(error) {
+      return res.
+        status(status.INTERNAL_SERVER_ERROR).
+        json({ error: error.toString() });
+    }
+
+    var json = {};
+    json[property] = result;
+    res.json(json);
+  };
+
+
+
+
   api.get('/category/id/:id', wagner.invoke(function(Category) {
     return function(req, res) {
-      Category.findOne({ _id: req.params.id }, function(error, category) {
-        if(error) {
-          return res.
-            status(status.INTERNAL_SERVER_ERROR).
-            json({ error: error.toString() });
-        }
-        if(!category) {
-          return res.
-            status(status.NOT_FOUND).
-            json({ error: 'Not found' });
-        }
-        res.json({ category: category});
-      })
+      Category.findOne({ _id: req.params.id }, handleOne.bind(null, 'category', res));
     };
   }));
 
@@ -27,14 +48,13 @@ module.exports = function(wagner) {
       Category.
         find({ parent: req.params.id }).
         sort({ _id: 1 }).
-        exec(function(error, categories) {
-        if(error) {
-          return res.
-            status(status.INTERNAL_SERVER_ERROR).
-            json({ error: error.toString() });
-        }
-        res.json({ categories: categories});
-      });
+        exec(handleMany.bind(null, 'categories', res));
+    };
+  }));
+
+  api.get('/product/id/:id', wagner.invoke(function(Product) {
+    return function(req, res) {
+      Product.findOne({ _id: req.params.id }, handleOne.bind(null, 'product', res));
     };
   }));
 
