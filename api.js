956d7707ff053f0a0ebc077368be7b3242ee351b
marcoaam <marco.army@gmail.com>
2016-05-07 21:20:37 +0100

    Added user cart functionality

--- api.js
+++ api.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var status = require('http-status');
+var bodyparser = require('body-parser');
 
 module.exports = function(wagner) {
   var api = express.Router();
+
+  api.use(bodyparser.json());
 
   var handleOne = function(property, res, error, result) {
       if(error) {
@@ -74,5 +77,33 @@ module.exports = function(wagner) {
     };
   }));
 
+  api.put('me/cart', wagner.invoke(function(User) {
+    return fuction(req, res) {
+      try {
+        var cart = req.body.data.cart;
+      } catch(e) {
+        return res.status(status.BAD_REQUEST).
+        json({ error: 'No cart specified' });
+      }
+
+      req.user.data.cart = cart;
+      req.user.save(function(error, user) {
+        if(error) {
+          return res.status(status.INTERNAL_SERVER_ERROR).
+                  json({ error: error.toString() });
+        }
+        return res.json({ user: user });
+      });
+    };
+  }));
+
+  api.get('me', function(req, res) {
+    if(!req.user) {
+      return res.status(status.UNAUTHORIZED).json({ error: 'Not logged in' });
+    }
+    req.user.populate(
+      { path: 'data.cart.product', model: 'Product' },
+      handleOne.bind(null, 'user', res));
+  });
   return api;
 };
